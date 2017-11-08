83fb7561b5d2d52a540d708fa1e5df6b65737efd
Philipe Godoy <mbx.philipe@gmail.com>
2017-11-08 14:15:31 -0200

    Implemetando a nossa feature de tela

--- engine.rb
+++ engine.rb
@@ -1,13 +1,16 @@
 load "input.rb"
+load "screen.rb"
 
 class Engine 
   def run 
+  	screen = Screen.new
   	while true
   	  # Le a entrada do usuario	
   	  movement = read_movement
   	  # para de executar o programa caso algo alem das setinhas seja recebido
    	  break if movement == "quit"   
-   	  puts "Running #{movement}"
+   	  # desenha um novo frame
+   	  screen.draw   	  
 	end
   end
 end
---
+++ screen.rb
@@ -0,0 +1,12 @@
+class Screen
+  def draw
+  	10.times do 
+		10.times do 
+			print "[ ]"
+		end
+		print "\n"
+  	end 
+  	puts "-----"
+  end 
+end
+
